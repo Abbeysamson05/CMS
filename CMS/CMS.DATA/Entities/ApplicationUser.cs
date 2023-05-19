e4dfab4e4738881eb3d9812998c65857eec8bd46
Kimbill <ayooluwaa@decagonhq.com>
2023-05-19 18:22:42 +0100

    added entities

--- CMS/CMS.DATA/Entities/ApplicationUser.cs
+++ CMS/CMS.DATA/Entities/ApplicationUser.cs
@@ -5,9 +5,14 @@ namespace CMS.DATA.Entities
 {
 	public class ApplicationUser:IdentityUser
 	{
-		public ApplicationUser()
-		{
-		}
-	}
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
 }
 
