7c5e8c45d2f806c569465766622dd1ff211681e2
Kaosilisochukwu <50839130+Kaosilisochukwu@users.noreply.github.com>
2023-05-22 14:47:54 +0100

    Update Quiz

--- CMS/CMS.DATA/Entities/Quiz.cs
+++ CMS/CMS.DATA/Entities/Quiz.cs
@@ -5,11 +5,12 @@
         public string Question { get; set; }
         public string AnswerType { get; set; }
         public string PreferedAnswer { get; set; }
-        public string AddedBy { get; set; }
+        public string AddedById { get; set; }
+        public ApplicationUser AddedBy { get; set; }
         public string Instruction { get; set; }
         public Lesson Lesson { get; set; }
         public string LessonId { get; set; }
         public List<QuizOption> QuizOptions { get; set; }
-        public List<UserQuizTaken> User { get; set; }
+        public List<UserQuizTaken> UserQuizzesTaken { get; set; }
     }
 }
