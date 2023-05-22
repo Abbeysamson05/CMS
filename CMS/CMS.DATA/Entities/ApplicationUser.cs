13553af02ca37103da5a24ac04dac47e7315a90f
Ayooluwa Ajibola-Thomas <86775288+Kimbill@users.noreply.github.com>
2023-05-22 14:35:03 +0100

    Update ApplicationUser.cs
    changed UpdatedAt to DateCreated and
    CreatedAt to DateUpdatww

--- CMS/CMS.DATA/Entities/ApplicationUser.cs
+++ CMS/CMS.DATA/Entities/ApplicationUser.cs
@@ -9,8 +9,8 @@ namespace CMS.DATA.Entities
         public string SquadNumber { get; set; }
         public string? PublicId { get; set; }
         public string? ImageUrl { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
         public bool ActiveStatus { get; set; }
         public string RefreshToken { get; set; } = String.Empty;
         public DateTime RefreshTokenExpiryTime { get; set; }
