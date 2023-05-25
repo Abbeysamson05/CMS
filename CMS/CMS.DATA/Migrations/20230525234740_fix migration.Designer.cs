882a77f89180872f30b5a71ce1056b581481e5df
Saheed Olawale Babatunde <bsaheed79@gmail.com>
2023-05-26 00:49:52 +0100

    fix folder structure

--- CMS/CMS.DATA/Migrations/20230525234740_fix migration.Designer.cs
+++ CMS/CMS.DATA/Migrations/20230525234740_fix migration.Designer.cs
@@ -12,8 +12,8 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CMS.DATA.Migrations
 {
     [DbContext(typeof(CMSDbContext))]
-    [Migration("20230522141622_EntityModificationMigration")]
-    partial class EntityModificationMigration
+    [Migration("20230525234740_fix migration")]
+    partial class fixmigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -23,6 +23,36 @@ namespace CMS.DATA.Migrations
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
+
+            modelBuilder.Entity("CMS.DATA.Entities.Activity", b =>
+                {
+                    b.Property<string>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("text");
+
+                    b.Property<DateTime>("DateCreated")
+                        .HasColumnType("timestamp without time zone");
+
+                    b.Property<DateTime?>("DateUpdated")
+                        .HasColumnType("timestamp without time zone");
+
+                    b.Property<string>("Description")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.Property<bool>("IsDeleted")
+                        .HasColumnType("boolean");
+
+                    b.Property<string>("UserId")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("UserId");
+
+                    b.ToTable("Activities");
+                });
 
             modelBuilder.Entity("CMS.DATA.Entities.ApplicationUser", b =>
                 {
@@ -128,11 +158,21 @@ namespace CMS.DATA.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("text");
 
+                    b.Property<string>("ActivityId")
+                        .HasColumnType("text");
+
+                    b.Property<string>("AddedBy")
+                        .IsRequired()
+                        .HasColumnType("text");
+
                     b.Property<DateTime>("DateCreated")
                         .HasColumnType("timestamp without time zone");
 
                     b.Property<DateTime?>("DateUpdated")
                         .HasColumnType("timestamp without time zone");
+
+                    b.Property<bool>("IsCompleted")
+                        .HasColumnType("boolean");
 
                     b.Property<bool>("IsDeleted")
                         .HasColumnType("boolean");
@@ -142,6 +182,8 @@ namespace CMS.DATA.Migrations
                         .HasColumnType("character varying(150)");
 
                     b.HasKey("Id");
+
+                    b.HasIndex("ActivityId");
 
                     b.ToTable("Courses");
                 });
@@ -319,6 +361,45 @@ namespace CMS.DATA.Migrations
                     b.ToTable("QuizOptions");
                 });
 
+            modelBuilder.Entity("CMS.DATA.Entities.QuizReviewRequest", b =>
+                {
+                    b.Property<string>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("text");
+
+                    b.Property<DateTime>("DateCreated")
+                        .HasColumnType("timestamp without time zone");
+
+                    b.Property<DateTime?>("DateUpdated")
+                        .HasColumnType("timestamp without time zone");
+
+                    b.Property<bool>("IsDeleted")
+                        .HasColumnType("boolean");
+
+                    b.Property<string>("Notes")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.Property<string>("QuizId")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.Property<DateTime>("Timestamp")
+                        .HasColumnType("timestamp without time zone");
+
+                    b.Property<string>("UserId")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("QuizId");
+
+                    b.HasIndex("UserId");
+
+                    b.ToTable("QuizReviewRequest");
+                });
+
             modelBuilder.Entity("CMS.DATA.Entities.Stack", b =>
                 {
                     b.Property<string>("Id")
@@ -347,6 +428,9 @@ namespace CMS.DATA.Migrations
                     b.Property<string>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("text");
+
+                    b.Property<bool>("CompletionStatus")
+                        .HasColumnType("boolean");
 
                     b.Property<string>("CourseId")
                         .IsRequired()
@@ -576,6 +660,24 @@ namespace CMS.DATA.Migrations
                     b.ToTable("AspNetUserTokens", (string)null);
                 });
 
+            modelBuilder.Entity("CMS.DATA.Entities.Activity", b =>
+                {
+                    b.HasOne("CMS.DATA.Entities.ApplicationUser", "User")
+                        .WithMany()
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("User");
+                });
+
+            modelBuilder.Entity("CMS.DATA.Entities.Course", b =>
+                {
+                    b.HasOne("CMS.DATA.Entities.Activity", null)
+                        .WithMany("Courses")
+                        .HasForeignKey("ActivityId");
+                });
+
             modelBuilder.Entity("CMS.DATA.Entities.Lesson", b =>
                 {
                     b.HasOne("CMS.DATA.Entities.ApplicationUser", "AddedBy")
@@ -625,10 +727,29 @@ namespace CMS.DATA.Migrations
                     b.Navigation("Quiz");
                 });
 
+            modelBuilder.Entity("CMS.DATA.Entities.QuizReviewRequest", b =>
+                {
+                    b.HasOne("CMS.DATA.Entities.Quiz", "Quiz")
+                        .WithMany()
+                        .HasForeignKey("QuizId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("CMS.DATA.Entities.ApplicationUser", "User")
+                        .WithMany("QuizReviews")
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Quiz");
+
+                    b.Navigation("User");
+                });
+
             modelBuilder.Entity("CMS.DATA.Entities.UserCourse", b =>
                 {
                     b.HasOne("CMS.DATA.Entities.Course", "Course")
-                        .WithMany("AddedBy")
+                        .WithMany()
                         .HasForeignKey("CourseId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
@@ -733,11 +854,18 @@ namespace CMS.DATA.Migrations
                         .IsRequired();
                 });
 
+            modelBuilder.Entity("CMS.DATA.Entities.Activity", b =>
+                {
+                    b.Navigation("Courses");
+                });
+
             modelBuilder.Entity("CMS.DATA.Entities.ApplicationUser", b =>
                 {
                     b.Navigation("Courses");
 
                     b.Navigation("Lessons");
+
+                    b.Navigation("QuizReviews");
 
                     b.Navigation("Quizes");
 
@@ -746,8 +874,6 @@ namespace CMS.DATA.Migrations
 
             modelBuilder.Entity("CMS.DATA.Entities.Course", b =>
                 {
-                    b.Navigation("AddedBy");
-
                     b.Navigation("Lessons");
                 });
 
