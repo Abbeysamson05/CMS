d121964acafdfaf6c897f118db88d0dc057a1f5c
stephen4018 <stephenohaeri4018@gmail.com>
2023-05-31 10:15:02 +0100

    added quiz page and learning content page

--- CMS/CMS.MVC/Controllers/ClassroomController.cs
+++ CMS/CMS.MVC/Controllers/ClassroomController.cs
@@ -6,11 +6,15 @@ namespace CMS.MVC.Controllers
     {
         public bool toggleState { get; set; } = false;
 
+        [HttpGet]
         public IActionResult LearningContent()
         {
+            ViewBag.ShowSuccessModal = toggleState;
+
             return View();
         }
 
+        [HttpGet]
         public IActionResult QuizPage()
         {
             ViewBag.ShowSuccessModal = toggleState;
