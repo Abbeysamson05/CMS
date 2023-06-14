0aa57be02a762c841e57e6d50cb760a1afc3e32e
Saheed Olawale Babatunde <bsaheed79@gmail.com>
2023-06-14 18:18:29 +0100

    fix get and set

--- CMS/CMS.MVC/Models/ConstantSubBaseEnpoint.cs
+++ CMS/CMS.MVC/Models/ConstantSubBaseEnpoint.cs
@@ -2,7 +2,7 @@
 {
     public static class ConstantSubBaseEnpoint
     {
-        public static string GetLessonUri { get; set; } = "api/lesson/all";
-        public static string AddLessonUri { get; set; } = "api/lesson/add";
+        public static string GetLessonUri  = "api/lesson/all";
+        public static string AddLessonUri = "api/lesson/add";
     }
 }
