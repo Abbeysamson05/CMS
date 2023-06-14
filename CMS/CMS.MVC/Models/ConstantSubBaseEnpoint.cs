3b466b4f64f108a440b7e034df06ce9671ce3bf1
Saheed Olawale Babatunde <bsaheed79@gmail.com>
2023-06-14 22:46:16 +0100

    fix

--- CMS/CMS.MVC/Models/ConstantSubBaseEnpoint.cs
+++ CMS/CMS.MVC/Models/ConstantSubBaseEnpoint.cs
@@ -2,7 +2,7 @@
 {
     public static class ConstantSubBaseEnpoint
     {
-        public static string GetLessonUri  = "api/lesson/all";
-        public static string AddLessonUri = "api/lesson/add";
+        public const  string GetLessonUri  = "api/lesson/all";
+        public const string AddLessonUri = "api/lesson/add";
     }
 }
