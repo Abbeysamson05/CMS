25e9897adedafccdfda3cff883153957332c2d05
Saheed Olawale Babatunde <bsaheed79@gmail.com>
2023-06-19 15:57:14 +0100

    initial optimization

--- CMS/CMS.MVC/Models/ConstantSubBaseEnpoint.cs
+++ CMS/CMS.MVC/Models/ConstantSubBaseEnpoint.cs
@@ -2,7 +2,7 @@
 {
     public static class ConstantSubBaseEnpoint
     {
-        public const  string GetLessonUri  = "api/lesson/all";
+        public const string GetLessonUri  = "api/lesson/all";
         public const string AddLessonUri = "api/lesson/add";
     }
 }
